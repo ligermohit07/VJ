670fb18ed9cd3bb28482c740f59f963bd29c5363
ligermohit07 <mohittatu2007@gmail.com>
2025-07-05 15:33:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2691,8 +2691,8 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         if settings["button"]:
             cap = f"<b>Tʜᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ ☞ {search}\n\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ ☞ {message.from_user.mention}\n\nʀᴇsᴜʟᴛ sʜᴏᴡ ɪɴ ☞ {remaining_seconds} sᴇᴄᴏɴᴅs\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title} \n\n⚠️ ᴀꜰᴛᴇʀ 5 ᴍɪɴᴜᴛᴇꜱ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ 🗑️\n\n</b>"
         else:
-            cap = f"<b>ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ : {search}\nʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\nʀᴇsᴜʟᴛ sʜᴏᴡ ɪɴ ☞ {remaining_seconds} sᴇᴄᴏɴᴅs\n\nᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title} \n\n⚠️ ᴀꜰᴛᴇʀ 5 ᴍɪɴᴜᴛᴇꜱ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ 🗑️\n\n</b>"
-            cap+="<b><u>📂 ʏᴏᴜʀ ꜱᴇᴀʀᴄʜᴇᴅ ғɪʟᴇꜱ ᴀʀᴇ ʜᴇʀᴇ</u></b>\n\n"
+            cap = f"<b>🎬 ʀᴇꜱᴜʟᴛꜱ ꜰᴏʀ : {search}\n👤 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}\n⏰ ʀᴇsᴜʟᴛ sʜᴏᴡ ɪɴ : {remaining_seconds} sᴇᴄᴏɴᴅs\n\</b>"
+            cap+="<b>📂 ʏᴏᴜʀ ꜱᴇᴀʀᴄʜᴇᴅ ғɪʟᴇꜱ ᴀʀᴇ ʜᴇʀᴇ</b>\n\n"
             for file in files:
                 cap += f"<b>📕 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}\n\n</a></b>"
 
