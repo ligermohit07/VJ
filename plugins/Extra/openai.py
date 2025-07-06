fb6b8336f94c7ea59d6d80b3c73a9726077cf7e2
ligermohit07 <mohittatu2007@gmail.com>
2025-07-06 14:56:43 +0530

    Update openai.py

--- plugins/Extra/openai.py
+++ plugins/Extra/openai.py
@@ -1,14 +1,4 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @DevThanos02
+# Subscribe YouTube Channel For Amazing Bot @DevThanos02
+# Ask Doubt on telegram @DevThanos02
 
-from pyrogram import Client, filters
-from plugins.Extra.engine import ask_ai
-
-
-@Client.on_message(filters.command('openai'))
-async def openai_ask(client, message):
-    if len(message.command) == 1:
-       return await message.reply_text("Give an input!")
-    m = await message.reply_text("👀")
-    await ask_ai(client, m, message)
