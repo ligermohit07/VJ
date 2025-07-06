3c1640469992ac6b792b29a7110145b585b1dd54
ligermohit07 <mohittatu2007@gmail.com>
2025-07-06 20:02:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2790,7 +2790,7 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
     SPELL_CHECK[mv_id] = movielist
     if AI_SPELL_CHECK == True and vj_search == True:
         vj_search_new = False
-        vj_ai_msg = await reply_msg.edit_text("<b><i>I Am Trying To Find Your Movie With Your Wrong Spelling.</i></b>")
+        vj_ai_msg = await reply_msg.edit_text("<b>𝗔𝗰𝘁𝗶𝘃𝗮𝘁𝗲 𝗦𝘂𝗽𝗽𝗲𝗿 𝗦𝗽𝗲𝗹𝗹 𝗖𝗵𝗲𝗰𝗸 👑</b>")
         movienamelist = []
         movienamelist += [movie.get('title') for movie in movies]
         for techvj in movienamelist:
